6bde5c92faa38eadde39f897306711a58616f7f8
Jack Rosenthal <jack@rosenth.al>
2016-02-16 23:53:41 -0700

    add system_info

--- src/info/system_info.cpp
+++ src/info/system_info.cpp
@@ -11,12 +11,43 @@ using namespace std;
 
 
 double get_uptime() {
-    // TODO: implement me
-    return 0.0;
+    FILE *fp = fopen(PROC_ROOT "/uptime", "r");
+    if (!fp) {
+        perror("uptime info");
+        exit(1);
+    }
+    double uptime;
+    fscanf(fp, "%lf", &uptime);
+    fclose(fp);
+    return uptime;
 }
 
 
 SystemInfo get_system_info() {
-    // TODO: implement me
-    return SystemInfo();
+    SystemInfo info;
+    FILE *fp = fopen(PROC_ROOT "/stat", "r");
+    char item[25];
+    while(!feof(fp) && !ferror(fp)) {
+        fscanf(fp, "%s ", item);
+        if (!strcmp(item, "procs_running")) {
+            fscanf(fp, "%u", &(info.num_running));
+        }
+        fscanf(fp, "%*[^\n]");
+    }
+    fclose(fp);
+    info.load_average = get_load_average();
+    info.cpus = get_cpu_info();
+    info.processes = get_all_processes(PROC_ROOT);
+    info.uptime = get_uptime();
+    info.num_processes = info.processes.size();
+    info.num_user_threads = 0;
+    info.num_kernel_threads = 0;
+    for (ProcessInfo proc: info.processes) {
+        for (ProcessInfo th: proc.threads) {
+            if (th.is_user_thread()) info.num_user_threads++;
+            else if (th.is_kernel_thread()) info.num_kernel_threads++;
+        }
+    }
+    info.num_threads = info.num_user_threads + info.num_kernel_threads;
+    return info;
 }
